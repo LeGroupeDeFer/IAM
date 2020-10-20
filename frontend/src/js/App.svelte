57a973a6b647841f86f336e80cea9d699355ccc9
Thibaut Septon <tsepton@gmail.com>
2020-10-20 17:19:16 +0200

    Add routing

--- frontend/src/js/App.svelte
+++ frontend/src/js/App.svelte
@@ -1,41 +1,11 @@
 <script>
-	import Login from './components/auth/Login.svelte';
-	export let version;
+  import Login from "./pages/auth/Login.svelte";
+  import { Router, Route } from "svelte-routing";
+
+  export let url;
+  export let version;
 </script>
 
-<main>
-	<div class="content text-center">
-		<p class="text-muted my-0">v{version}</p>
-		<h1 class="text-primary py-0 my-0">IAM</h1>
-		<a href="https://github.com/LeGroupeDeFer/IAM" class="git-link">
-			View on HitHub
-		</a>
-
-		<hr />
-		<Login />
-	</div>
-</main>
-
-<style>
-	.content {
-		width: 300px;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-	}
-
-	h1 {
-		font-size: 5em;
-	}
-
-	.git-link {
-		color: #62929E;
-	}
-
-	.git-link:hover {
-		text-decoration: none;
-		border-bottom: 3px solid #393D3F;
-		padding-bottom: 3px;
-	}
-</style>
+<Router {url}>
+  <Route path="/"><Login {version} /></Route>
+</Router>
