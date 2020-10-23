ad7813fba5273318f2ba7c633e29c8d8d84370b8
Thibaut Septon <tsepton@gmail.com>
2020-10-23 12:11:25 +0200

    Init mapbox

--- frontend/src/js/pages/dashboard/Dashboard.svelte
+++ frontend/src/js/pages/dashboard/Dashboard.svelte
@@ -1,8 +1,19 @@
 <script>
   import Navbar from "../../components/overall/Navbar.svelte";
+  import Map from './Map.svelte';
+	import MapMarker from './MapMarker.svelte';
 </script>
 
+<style>
+  .mapbox {
+    height: 75vh;
+    margin: 1rem;
+  }
+</style>
+
 <Navbar />
-<main>
-  <b>Dashboard TODO</b>
-</main>
+<div class='mapbox'>
+  <Map lat={50.4667} lon={4.8667} zoom={14.5}>
+    <MapMarker lat={50.465856} lon={4.857599} label="UNamur Eco-inf cans"/>
+  </Map>
+</div>
---
+++ frontend/src/js/pages/dashboard/mapbox.js
@@ -0,0 +1,8 @@
+import mapbox from 'mapbox-gl';
+
+// https://docs.mapbox.com/help/glossary/access-token/
+mapbox.accessToken = "Your API token";
+
+const key = {};
+
+export { mapbox, key };
