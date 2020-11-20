e8f9335319a6038c8c94c7f7c881589487388407
latour-gui <guillaume.latour@student.unamur.be>
2020-11-20 09:45:22 +0100

    rework raspberry sync

--- raspberry/phidgets.py
+++ raspberry/phidgets.py
@@ -15,29 +15,52 @@ channels = {
     7: '2Y0A21 F08'
 }
 
+
+def reset_buffered_data() -> dict:
+    return {6: [], 7: []}
+
+
 last_sync = datetime.now().timestamp()
 sync_interval = 30  # (sec)
+buffered_data = reset_buffered_data()
 
 with open('parameters.json') as json_file:
     data = json.load(json_file)
 
 
 def on_sharp_change(self, sensor_value, _sensor_unit):
-    global channels, data, last_sync, sync_interval
-
-    # prevent sync overflow
-    if last_sync + sync_interval > datetime.now().timestamp():
-        return
+    global channels, data, last_sync, sync_interval, buffered_data
 
     model = channels[self.getChannel()]
     # if f(x) = m*x + c
     # and we get sensor_value as f(x) (or y)
     # to find x, we must calculate (y-c)/m
     corrected_value = (float(sensor_value) - data[model]['c']) / data[model]['m']
+    buffered_data[self.getChannel()].append(corrected_value)
+
+    # prevent sync overflow
+    if last_sync + sync_interval <= datetime.now().timestamp():
+        send_sync_message()
+        buffered_data = reset_buffered_data()
+        last_sync = datetime.now().timestamp()
+
+
+def send_sync_message():
+    global data, buffered_data, channels
     can_max_height = data['can_max_height']
 
+    avg = 0
+    for index, values in buffered_data.values():
+        if channels[index] == '2Y0A21 F08':
+            t = [v for v in values if v < 60]
+            specific_avg = sum(t) / len(t)
+        else:
+            specific_avg = sum(values) / len(values)
+        avg += specific_avg
+    avg = avg / len(buffered_data)
+
     # get the filling rate between 0 and 1
-    filling_rate = 1 - corrected_value / can_max_height
+    filling_rate = 1 - avg / can_max_height
     if filling_rate < 0:
         filling_rate = 0
     if filling_rate > 1:
