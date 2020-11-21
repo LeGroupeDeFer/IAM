d3fcba09fd300b7c2bed78755f670366556f6b6a
findot <findot@protonmail.com>
2020-11-21 20:20:45 +0100

    [Backend] - Added new migration directory

---
+++ backend/src/main/resources/db/migration/V2__add_cans_data.sql
@@ -0,0 +1,13 @@
+ALTER TABLE cans ADD deleted_at TIMESTAMP NULL;
+ALTER TABLE cans ADD identifier VARCHAR(50) NOT NULL UNIQUE AFTER id;
+
+
+CREATE TABLE can_data (
+    id              INT UNSIGNED NOT NULL  AUTO_INCREMENT,
+    can_id          INT UNSIGNED NOT NULL,
+    moment   TIMESTAMP NOT NULL DEFAULT NOW(),
+    filling_rate    DOUBLE(5,3) NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (can_id) REFERENCES cans (id)
+);
